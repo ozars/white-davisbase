a390895268a78d10425d4ddb768b9006f372349c
Omer Ozarslan <omer@utdallas.edu>
2019-04-28 14:05:21 -0500

    Fix code formatting issues

--- src/fs.cpp
+++ src/fs.cpp
@@ -17,128 +17,95 @@ Cell::Cell(Page& page, const char* base_addr) : pimpl(base_addr ? std::make_uniq
 
 PageNo Cell::leftChildPageNo() const {
   if (pimpl->page.isLeaf())
-    throw std::runtime_error("Left child page no is valid only for cells in interior pages");
+    throw std::runtime_error(
+      "Left child page no is valid only for cells in interior pages");
   return *reinterpret_cast<PageNo*>(pimpl->base_addr);
 }
 
 PayloadSize Cell::payloadSize() const {
   if (pimpl->page.isTable()) {
     if (pimpl->page.isLeaf())
-      return *reinterpret_cast<PayloadSize*>
-    else
-      throw std::runtime_error("Payload size is not valid for interior table pages");
+      return *reinterpret_cast<PayloadSize*> else throw std::runtime_error(
+        "Payload size is not valid for interior table pages");
   if (pimpl->page.
-}
-RowId Cell::rowid() const {
-}
+  }
+  RowId Cell::rowid() const {}
 
-IndexData Cell::indexData() const {
-}
-RowData Cell::rowData() const {
-}
+  IndexData Cell::indexData() const {}
+  RowData Cell::rowData() const {}
 
-void Cell::writeIndexData(const IndexData& index_data) {
-}
-void Cell::writeRowData(const RowData& row_data) {
-}
+  void Cell::writeIndexData(const IndexData& index_data) {}
+  void Cell::writeRowData(const RowData& row_data) {}
 
-struct Page::impl {
-};
+  struct Page::impl
+  {};
 
-Page::Page(Table& table, std::unique_ptr<char[]> raw_data, PageNo parent_page_no) {
-}
+  Page::Page(Table & table, std::unique_ptr<char[]> raw_data,
+             PageNo parent_page_no)
+  {}
 
-PageNo Page::pageNo() const {
-}
-PageNo Page::parentPageNo() const {
-}
-PageNo Page::rightmostChildPageNo() const {
-}
-PageNo Page::rightSiblingPageNo() const {
-}
+  PageNo Page::pageNo() const {}
+  PageNo Page::parentPageNo() const {}
+  PageNo Page::rightmostChildPageNo() const {}
+  PageNo Page::rightSiblingPageNo() const {}
 
-void Page::setPageNo(PageNo page_no) {
-}
-void Page::setParentPageNo(PageNo parent_page_no) {
-}
-void Page::setRightmostChildPageNo(PageNo rightmost_child_page_no) {
-}
-void Page::setRightSiblingPageNo(PageNo right_sibling_page_no) {
-}
+  void Page::setPageNo(PageNo page_no) {}
+  void Page::setParentPageNo(PageNo parent_page_no) {}
+  void Page::setRightmostChildPageNo(PageNo rightmost_child_page_no) {}
+  void Page::setRightSiblingPageNo(PageNo right_sibling_page_no) {}
 
-bool Page::isRoot() const {
-}
-bool Page::isLeaf() const {
-}
-bool Page::isTable() const {
-}
+  bool Page::isRoot() const {}
+  bool Page::isLeaf() const {}
+  bool Page::isTable() const {}
 
-int Page::cellCount() const {
-}
-Cell Page::getCell(int cell_index) const {
-}
+  int Page::cellCount() const {}
+  Cell Page::getCell(int cell_index) const {}
 
-bool Page::hasEnoughSpace(const RowData& row_data) {
-}
-Cell Page::addCell(const RowData& row_data, RowId row_id) {
-}
+  bool Page::hasEnoughSpace(const RowData& row_data) {}
+  Cell Page::addCell(const RowData& row_data, RowId row_id) {}
 
-bool Page::hasEnoughSpace() {
-}
-Cell Page::addCell(PageNo left_child_page, RowId row_id) {
-}
+  bool Page::hasEnoughSpace() {}
+  Cell Page::addCell(PageNo left_child_page, RowId row_id) {}
 
-bool Page::hasEnoughSpace(const IndexData& index_data) {
-}
-Cell Page::addCell(const IndexData& index_data, PageNo left_child_page_no) {
-}
+  bool Page::hasEnoughSpace(const IndexData& index_data) {}
+  Cell Page::addCell(const IndexData& index_data, PageNo left_child_page_no) {}
 
-const char* Page::rawData() const {
-}
-void Page::commit() {
-}
+  const char* Page::rawData() const {}
+  void Page::commit() {}
 
-struct Table::impl {
-}
+  struct Table::impl
+  {
+  }
 
-Table::Table(Database& database, std::string name, std::istream file, PageNo root_page_no, RowId next_row_id, PageSize page_size) {
-}
+  Table::Table(Database & database, std::string name, std::istream file,
+               PageNo root_page_no, RowId next_row_id, PageSize page_size)
+  {}
 
-PageNo Table::rootPageNo() const {
-}
-RowId Table::nextRowId() const {
-}
-PageSize Table::pageSize() const {
-}
-PageCount Table::pageCount() const {
-}
+  PageNo Table::rootPageNo() const {}
+  RowId Table::nextRowId() const {}
+  PageSize Table::pageSize() const {}
+  PageCount Table::pageCount() const {}
 
-Page Table::getPage(PageNo page_no) const {
-}
+  Page Table::getPage(PageNo page_no) const {}
 
-void Table::commitPage(const Page& page) {
-}
-void Table::setRootPageNo(PageNo page_no) {
-}
-void Table::setNextRowId(RowId next_row_id) {
-}
+  void Table::commitPage(const Page& page) {}
+  void Table::setRootPageNo(PageNo page_no) {}
+  void Table::setNextRowId(RowId next_row_id) {}
 
-static Page Table::createInteriorPage(PageNo page_no, PageNo rightmost_child_page_no, PageNo parent_page_no) {
-}
+  static Page Table::createInteriorPage(
+    PageNo page_no, PageNo rightmost_child_page_no, PageNo parent_page_no)
+  {}
 
-static Page Table::createLeafPage(PageNo page_no, PageNo right_sibling_page_no, PageNo parent_page_no) {
-}
+  static Page Table::createLeafPage(
+    PageNo page_no, PageNo right_sibling_page_no, PageNo parent_page_no)
+  {}
 
-struct Database::impl {
-};
-Database::Database(std::string directory_path, PageSize default_page_size) {
-}
+  struct Database::impl
+  {};
+  Database::Database(std::string directory_path, PageSize default_page_size) {}
 
-Table Database::createTable(std::string table_name) {
-}
-Table Database::getTable(std::string table_name) const {
-}
-void Database::removeTable(std::string table_name) {
-}
+  Table Database::createTable(std::string table_name) {}
+  Table Database::getTable(std::string table_name) const {}
+  void Database::removeTable(std::string table_name) {}
 
 } // namespace white::davisbase::fs
