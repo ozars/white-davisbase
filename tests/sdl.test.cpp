a3756e6d447cf1259be31179faa44615684b6825
Omer Ozarslan <omer@utdallas.edu>
2019-04-29 15:29:14 -0500

    Add sdl stub test file

---
+++ tests/sdl.test.cpp
@@ -0,0 +1,10 @@
+#include "catch2/catch.hpp"
+
+#include <filesystem>
+
+#include "sdl/column_value.cpp"
+#include "sdl/database.cpp"
+#include "sdl/page.cpp"
+#include "sdl/table.cpp"
+#include "sdl/table_interior_page.cpp"
+#include "sdl/table_leaf_page.cpp"
