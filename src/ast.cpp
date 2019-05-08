71359ed14fbfeb08957b33e26cc1d526de5bb7c3
Ritwick Sv <43278208+RitwickSV@users.noreply.github.com>
2019-05-08 09:35:58 -0500

    Initial code for update command execution

--- src/ast.cpp
+++ src/ast.cpp
@@ -261,6 +261,65 @@ void DeleteFromCommand::execute(Database& database)
 void UpdateCommand::execute(Database& database)
 {
   std::cout << *this << std::endl;
+  bool found = false;
+  bool is_col_unique = false;
+  bool can_update = true;
+
+  auto table_opt = database.getTable(table_name);
+  //if(!table_opt.has_value())
+  if(table_opt==std::nullopt)
+   throw std::runtime_error("The table doesn't exist");
+  auto& table = table_opt.value();
+  auto& col_def = table.columnDefinitions();
+  
+  std::optional<size_t> column_index_opt, column_where_index_opt;
+
+  if (condition.has_value()) {
+    auto& cond = condition.value();
+    for(size_t i=0; i<col_def.size(); i++)
+    {
+      if(cond.column_name == col_def[i].name)
+          column_where_index_opt = i;
+    }
+
+    if(!column_where_index_opt.has_value())
+      throw std::runtime_error("The column doesn't exist in the table");
+  }
+  
+  for(size_t i=0; i<col_def.size(); i++)
+  {
+    if(column_name == col_def[i].name){
+      if (col_def[i].modifiers.unique || col_def[i].modifiers.primary_key)
+        is_col_unique = true;
+      column_index_opt = i;
+    }
+  }
+
+  if(!column_index_opt.has_value())
+   throw std::runtime_error("The column doesn't exist in the table");
+
+  auto& column_index = column_index_opt.value();
+  auto& column_where_index = column_where_index_opt.value();
+
+  table.mapOverRecords([&](TableLeafCell cell){
+    auto& cell_row_data = cell.row_data;
+    if(cell_row_data[column_index]==value)
+      can_update = false;
+  });
+
+  table.mapOverRecords([&](TableLeafPage& page, TableLeafCell record){
+    auto& row_data = record.row_data;
+    if (!condition.has_value() || isWhereSatisfied(row_data[column_where_index], condition.value()))
+    {
+      if(can_update)
+      {
+        row_data[column_index] = value;
+        page.updateRecord(record);
+      }
+      else
+          throw std::runtime_error("The column is unique and the value already exists in the table");   
+    }
+  });
 }
 
 void CreateIndexCommand::execute(Database& database)
